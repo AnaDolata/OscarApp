a6c8b42c77c21632af478f4bef4085ab506fe311
Guilherme Janke <guilhermejanke15@gmail.com>
2022-04-30 16:10:03 -0300

    DEV - JSON

--- app/src/main/java/com/example/oscarapp/VoteMovie.java
+++ app/src/main/java/com/example/oscarapp/VoteMovie.java
@@ -32,7 +32,7 @@ public class VoteMovie extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_vote_movie);
 
         recyclerViewMovies = findViewById(R.id.recyclerViewMovies);
 
